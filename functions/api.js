135ef25131023010f4b3e9fbb53a6ddf82b865ff
Airly12000 <littlefrancis28@gmail.com>
2023-06-28 15:39:53 +0000

    netlify deploy

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
-const Invoices = require('../api/routes/Invoices');
-const mysql = require('mysql2');
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../', '.env') });
+const Invoices = require('../routes/Invoices');
+// const mysql = require('mysql2');
+// const path = require('path');
+// require('dotenv').config({ path: path.join(__dirname, '../', '.env') });
 
 const app = express();
 
@@ -12,13 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-process.env.NODE_ENV = 'production';
-
 // Express Routes
 app.use('/api/routes', Invoices);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Sever running on port ${PORT}`));
 
 module.exports.handler = serverless(app);
